ef37a491e9d24f75958bf13ca98f68af643d8863
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2020-03-04 17:06:43 -0500

    navbar, logo, icons

---
+++ public/js/nav.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.navbar-burger').addEventListener('click', burgerClickHandler)
+})
+
+function burgerClickHandler(element) {
+  document.querySelector('.navbar-menu').classList.toggle('is-active')
+}
