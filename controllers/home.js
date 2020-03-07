3a3e2cd2bf90a78c30669c08428536b59a2a8dd9
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2020-03-06 20:59:52 -0500

    get event info from meetup api, event cards on homepage

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,27 @@
+const _ = require('lodash')
+const moment = require('moment')
+const reqp = require('request-promise')
+
+const NUM_UPCOMING_EVENTS = 5
+
 function homepage(req, res) {
-  res.render('home')
+  reqp({ uri: 'http://api.meetup.com/tjex-ca/events',
+    qs: {
+      offset: 0,
+      page: NUM_UPCOMING_EVENTS,
+      status: 'upcoming'
+    },
+    json: true
+  })
+  .then((api_res) => {
+    let events = _.chain(api_res)
+      .map((event) => {
+        event.formatted_starttime = moment(event.time).format('ddd, MMM D, h:mm a')
+        return event
+      })
+      .value()
+    res.render('home', { events })
+  })
 }
 
 module.exports = {
