b3ef6c0005b1954a81622a2158a6be3636dc27f4
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2020-03-13 12:43:04 -0400

    add favicon

--- app/app.js
+++ app/app.js
@@ -3,11 +3,13 @@ const config = require('config')
 const sassMiddleware = require('node-sass-middleware')
 const compression = require('compression')
 const helmet = require('helmet')
+const favicon = require('serve-favicon')
 
 const app = express()
 
 app.use(compression())
 app.use(helmet())
+app.use(favicon('public/img/favicon.ico', { maxAge: 31536000000 }))
 app.set('views', 'public/views')
 app.set('view engine', 'ejs')
 app.use(sassMiddleware({
