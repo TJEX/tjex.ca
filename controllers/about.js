a527226baf7761a49b326da138c333e8d79f442f
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2020-03-14 15:01:01 -0400

    separate staff data, add links to meetup profile

--- controllers/about.js
+++ controllers/about.js
@@ -1,51 +1,8 @@
-const STAFF = [
-  {
-    name: "Mark",
-    main: true,
-    position: "Co-founder & Treasurer"
-  },
-  {
-    name: "Forough",
-    main: true,
-    position: "Head Organizer",
-  },
-  {
-    name: "Lona",
-  },
-  {
-    name: "Andy",
-  },
-  {
-    name: "Carlo",
-  },
-  {
-    name: "Ryan",
-  },
-  {
-    name: "Pek",
-  },
-  {
-    name: "Brandon",
-    image: "/img/staff/brandon.jpg",
-    description: "I made this website."
-  },
-  {
-    name: "Joe",
-  },
-  {
-    name: "Yuumi"
-  },
-  {
-    name: "Manami",
-  },
-  {
-    name: "Daiki",
-  }
-]
+const staff = require('../data/staff')
 
 function index(req, res) {
   res.setHeader('Cache-control', 'public, max-age=86400')
-  res.render('about', { STAFF })
+  res.render('about', { staff })
 }
 
 module.exports = {
